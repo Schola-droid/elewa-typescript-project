4fa3aedbf1ddf20ffd76d31ba09d975943d4136d
Scholastica Nzomo <scholambula1@gmail.com>
2023-11-08 17:50:05 +0300

    Done with about and invest

---
+++ elewa/src/invest/Othertext.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Othertext = () => {
+  return (
+    // Creating and adding stylings to our 'othertext' section then exporting the function to the main page of Invest page which is located at 'invest.tsx'
+    <div className='text-center'>
+        <p>Become part of our mission.</p>
+        <p>And start <b>investing</b></p>
+    </div>
+  )
+}
+
+export default Othertext
